c8d34bdb383285d9228848f01c9d3545302668d8
nisino25 <nisino25@gmail.com>
2024-02-10 23:21:45 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+   <h1>しんたままのサイト</h1>
+  <!-- <div ref="twitterEmbed"></div> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      twitterWidgetUrl: 'https://platform.twitter.com/widgets.js'
+    };
+  },
+  methods: {
+    
+    // loadTwitterWidget() {
+    //   if (!document.querySelector(`script[src="${this.twitterWidgetUrl}"]`)) {
+    //     const script = document.createElement('script');
+    //     script.setAttribute('src', this.twitterWidgetUrl);
+    //     script.setAttribute('async', '');
+    //     script.setAttribute('charset', 'utf-8');
+    //     document.head.appendChild(script);
+    //   }
+    // },
+    // addTimeline() {
+    //   const timeline = document.createElement('a');
+    //   timeline.className = 'twitter-timeline';
+    //   timeline.href = 'https://twitter.com/pokemongo_db?ref_src=twsrc%5Etfw';
+    //   timeline.innerText = 'Tweets by pokemongo_db';
+    //   this.$refs.twitterEmbed.appendChild(timeline);
+    // }
+  },
+  mounted() {
+    // this.loadTwitterWidget();
+    // this.addTimeline();
+    // this.$nextTick(() => {
+    //   window.twttr && window.twttr.widgets && window.twttr.widgets.load();
+    // });
+  }
+}
+</script>
