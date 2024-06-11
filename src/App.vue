d16f47a53c71597281822fdf8aa669279808ed6b
nisino25 <nisino25@gmail.com>
2024-06-12 08:47:42 +0900

    updaed 6-12 -v3

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,7 @@
       <div class="section section-location">
         <div class="section-inner aos-animation">
           <h3>- Access -</h3>
-          <h2>〒063-0811札幌市西区<br>琴似1条4丁目4-21タケダビル2F</h2>
+          <h2>〒063-0811札幌市西区琴似<br>1条4丁目4-21タケダビル2F</h2>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.314766162808!2d141.3043865!3d43.0768774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b284ea53ae893%3A0x4750c6f721882ee1!2z44CSMDYzLTA4MTEgSG9ra2FpZG8sIFNhcHBvcm8sIE5pc2hpIFdhcmQsIEtvdG9uaSAxIErFjSwgNC1jaMWNbWXiiJI04oiSMjEg44K_44Kx44OA44OT44Or!5e0!3m2!1sen!2sjp!4v1708440322308!5m2!1sen!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
